9d4f98019467e613d0097718479e466fab911295
brianclauzel <brianthomasclauzel@gmail.com>
2018-03-06 21:29:30 -0800

    adding more javascript for buttons

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,30 @@
 // api key: eb34671b691c4ff98e986ec16ee16beb 
+$(window).on("click", function(){
 
-var input = {
-    'api-key': "eb34671b691c4ff98e986ec16ee16beb",
-  'q': 'obama',
-  'begin_date': "20100601",
-  'end_date': "20100801"
-};
+    var q = $("#searchTerm").val().trim();
+    var begin = $("#startYear").val().trim();
+    var end = $("#endYear").val().trim();
+
+    var input = {
+        'api-key': "eb34671b691c4ff98e986ec16ee16beb",
+    'q': q,
+    'begin_date': begin,
+    'end_date': end
+    };
 
 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param(input);
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
 
-  console.log(result.response.docs[0]);
-}).fail(function(err) {
-  throw err;
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param(input);
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+
+        console.log(result.response.docs[0]);
+        }).fail(function(err) {
+        throw err;
+        });
+
 });
