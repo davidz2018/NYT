3146453d4b979434d900db514614973c30730f99
brianclauzel <brianthomasclauzel@gmail.com>
2018-03-06 21:21:46 -0800

    yolo

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,22 @@
 // api key: eb34671b691c4ff98e986ec16ee16beb 
 
-
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "eb34671b691c4ff98e986ec16ee16beb",
-  'q': "obama",
+var input = {
+    'api-key': "eb34671b691c4ff98e986ec16ee16beb",
+  'q': 'obama',
   'begin_date': "20100601",
   'end_date': "20100801"
-});
+};
+
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param(input);
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  console.log(result);
+
+  console.log(result.response.docs[0]);
 }).fail(function(err) {
   throw err;
 });
