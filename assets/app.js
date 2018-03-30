9ed2b4fff2b5b30bda55e2db538205b0b8e908d7
SLandgra <slandgra@ucsd.edu>
2018-03-29 19:53:08 -0700

    Added jquery functionality to display ajax response in app.js

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-// api key: eb34671b691c4ff98e986ec16ee16beb 
+// api key: eb34671b691c4ff98e986ec16ee16beb
 $("#findOne").on("click", function(){
 
     var q = $("#searchTerm").val().trim();
@@ -12,19 +12,40 @@ $("#findOne").on("click", function(){
         'end_date': end + '1231'
     };
 
-
-
-
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param(input);
     $.ajax({
         url: url,
         method: 'GET',
     }).done(function(result) {
-        $(".form2").append(result.response.docs[0].headline.main)
-        console.log(result.response.docs[0]);
+        var form = $(".form2");
+        var table = $('<table></table>');
+        if($('#numrecord').val().trim() === ''){
+          var numberOfArticles = result.response.docs.length;
+        }else{
+          var numberOfArticles = Number($('#numrecord').val().trim()) || -1;
+        }
+        if(numberOfArticles < 0){
+          return alert('Invalid input for Number displayed')
+        }
+        table.append('<tr><th>Headline</th><th>Publication Date</th><th>Link</th></tr>')
+        for(var i = 0; i<numberOfArticles; i++){
+          var row = $("<tr></tr>")
+          row.append('<td><a href=" '+ result.response.docs[i].web_url + '">'  + result.response.docs[i].headline.main + '</a></td>')
+          row.append('<td>' + result.response.docs[i].pub_date.slice(0,10) + '</td>')
+          table.append(row)
+        }
+
+        $(".form2").append(table)
+
         }).fail(function(err) {
-        throw err;
+          throw err;
         });
 
 });
+$("#clearOne").on("click", function(){
+  $("#searchTerm").val('');
+  $("#startYear").val('');
+  $("#endYear").val('');
+  $(".form2").empty();
+})
